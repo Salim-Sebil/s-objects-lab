6d35c531787a0210de83ad321ac56f2490710d1a
salim-sebil <salim.sebil@student.moringaschool.com>
2026-03-22 18:51:05 +0300

    adding index.js file

--- index.js
+++ index.js
@@ -1,6 +1,48 @@
 //Write your code here
 
+const attendee = {
 
+  attendeeId: "T001",
+  name: "Alice Smith",
+  event: "JavaScript Conference",
+  ticketType: "VIP",
+  ticketPrice: 150.00
+}
+
+//log atteend name function
+function logAttendeeName(attendee, name){
+  console.log(attendee.name)
+
+}
+
+//log ticket price function
+function logTicketPrice(attendee, ticketPrice){
+  console.log(attendee.ticketPrice)
+
+}
+
+//update ticket type function
+function updateTicketType(attendee, ordinary){
+  attendee.ticketType = ordinary
+
+}
+
+//update tickket price function
+function updateTicketPrice(attendee, newTicketPrice){
+
+  attendee.ticketPrice = newTicketPrice
+}
+
+//remove event property function
+function removeEventProperty(attendee){
+  delete attendee.event
+}
+
+
+//checked in property
+function addCheckedInProperty(attendee){
+  attendee.checkedIn = true
+}
 
 //Needed for the tests to work. Don't modify
 module.exports = {
